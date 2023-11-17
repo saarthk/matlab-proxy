c31053efd9140c04c223ab274f07d4c35fc87165
Saarthak Gupta <saarthag@mathworks.com>
2023-11-17 23:30:49 +0530

    added isTimeoutExpired state var in App, added TerminateWarning component

--- gui/src/components/App/index.js
+++ gui/src/components/App/index.js
@@ -13,6 +13,7 @@ import Controls from '../Controls';
 import Information from '../Information';
 import Help from '../Help';
 import Error from '../Error';
+import TerminateWarning from '../TerminateWarning';
 import {
   selectOverlayVisible,
   selectFetchStatusPeriod,
@@ -167,17 +168,12 @@ function App() {
         window.history.replaceState(null, '', `${baseUrl}index.html`); 
     }, [dispatch, baseUrl]);
     
+    const [isTimerExpired, setIsTimerExpired] = useState(false);
+
     let timerCancel, timerReset;
     [, timerCancel, timerReset] = useTimeoutFn(() => {
-        if (matlabStarting || (matlabUp && matlabBusy)) {
-            console.log("MATLAB starting/busy, resetting timer!");
-            timerReset();
-        // TODO: add cases for stopping (and other) states of MATLAB
-        } else {
-            dispatch(fetchTerminateIntegration());
-            // timerCancel();
-        }
-    }, idleTimeoutDurationInMS);
+        setIsTimerExpired(true);
+    });
 
     useEffect(() => {
         if ((!authEnabled || isAuthenticated) && isTimeoutEnabled) {
@@ -196,10 +192,14 @@ function App() {
     // * Status Information
     let overlayContent;   
 
-    if (dialog) {
+    // Show an impending termination warning if timeout is enabled and the timer has expired
+    if (isTimeoutEnabled && isTimerExpired) {
+        overlayContent = <TerminateWarning />;
+    }
+    else if (dialog) {
         // TODO Inline confirmation component build
         overlayContent = dialog;
-    }    
+    }
     // Give precedence to token auth over licensing info ie. once after token auth is done, show licensing if not provided.
     else if((!licensingProvided) && hasFetchedServerStatus && (!authEnabled || isAuthenticated)) {    
         overlayContent = <LicensingGatherer role="licensing" aria-describedby="license-window" />;
